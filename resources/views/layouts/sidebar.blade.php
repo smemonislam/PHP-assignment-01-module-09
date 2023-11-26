c27c6208db422f8cf57752e2eedc55e672703a2a
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-11-26 23:33:10 +0600

    Change active class when route change in sidebar menu

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,10 +7,10 @@
     <nav id="colorlib-main-menu" role="navigation" class="navbar">
         <div class="main-menu">
             <ul>
-                <li class="active"><a href="{{ url('/') }}">Home</a></li>
-                <li><a href="{{ route('about') }}">About Me</a></li>
-                <li><a href="{{ route('projects') }}">Projects</a></li>
-                <li><a href="{{ route('contact') }}">Contact</a></li>
+                <li class="{{ request()->is('/') ? 'active' : '' }}"><a href="{{ url('/') }}">Home</a></li>
+                <li class="{{ request()->routeIs('about') ? 'active' : '' }}"><a href="{{ route('about') }}">About Me</a></li>
+                <li class="{{ request()->routeIs('projects') ? 'active' : '' }}"><a href="{{ route('projects') }}">Projects</a></li>
+                <li class="{{ request()->routeIs('contact') ? 'active' : '' }}"><a href="{{ route('contact') }}">Contact</a></li>
             </ul>
         </div>
     </nav> 
